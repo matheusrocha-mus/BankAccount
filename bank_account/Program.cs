58f4b094ba67ca7c3cc3c898f5f6a3fd708cd859
Desenvolvimento de Sistemas <sis@educaserver.local>
2023-03-01 11:36:39 -0300

    first commit

---
+++ bank_account/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank_account
+{
+    internal class Program
+    {
+        public class Bank_Account
+        {
+            private string name;
+            private string ssn;
+            private double balance;
+            private string date;
+            private string aba;
+
+            public string SSN
+            {
+                get { return ssn; }
+                set
+                {
+                    if (int.TryParse(value, out int validSSN) & value.Length == 9)
+                    {
+                        ssn = Convert.ToString(validSSN);
+                    }
+                    else
+                    {
+                        ssn = null;
+                    }
+                }
+            }
+
+        }
+    }
+}
